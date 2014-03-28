3234c4107c8b6c944cc604085deed8f3e21b1db8
Zsolt Ero <zsolt.ero@gmail.com>
2014-03-28 20:51:09 +0000

    prepare file changed

--- prepare_system.sh
+++ prepare_system.sh
@@ -1,18 +1,20 @@
 #!/usr/bin/env bash
 
 # prepare package building
-sudo apt-get -y --no-install-recommends install build-essential curl debhelper devscripts fping gdebi libwxbase2.8-dev libwxgtk2.8-dev python python-dev python-gtk2 python-pygame python-webkit rxvt-unicode-256color udhcpc wx2.8-headers
+sudo apt-get -y --no-install-recommends install build-essential curl \
+    debhelper devscripts fping gdebi libwxbase2.8-dev libwxgtk2.8-dev python \
+    python-dev python-gtk2 python-pygame python-webkit rxvt-unicode-256color \
+    udhcpc wx2.8-headers
 
 # prepare python
-apt-get -y purge python-setuptools python-virtualenv python-pip
+sudo apt-get -y purge python-setuptools python-virtualenv python-pip
 wget -q --no-check-certificate https://raw.github.com/pypa/pip/master/contrib/get-pip.py -O get-pip.py
 python get-pip.py
 rm get-pip.py
 
 # install python modules
-pip install requests
+pip install --upgrade requests
 
-groupadd kanousers
-usermod -a -G kanousers root
-usermod -a -G kanousers user
+sudo groupadd kanousers
+sudo usermod -a -G kanousers $USER
 
