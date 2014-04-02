5ee9c937364d1f008651440c07303770b2b7fa45
Zsolt Ero <zsolt.ero@gmail.com>
2014-04-02 11:09:41 +0100

    token removed from install snake

--- install-programs/install-snake.sh
+++ install-programs/install-snake.sh
@@ -3,7 +3,7 @@
 # install kano-debber
 mkdir -p /home/user/kano-debber
 cd /home/user/kano-debber
-curl -H "Authorization: token 06e849a65eaf62f48b1fc4eb6085a71cbbd1db89" -L -o tmp.tgz https://api.github.com/repos/KanoComputing/kano-debber/tarball/master
+curl -L -o tmp.tgz https://api.github.com/repos/KanoComputing/kano-debber/tarball/master
 tar --strip-components 1 --show-transformed-names -xzvf tmp.tgz
 rm tmp.tgz
 
